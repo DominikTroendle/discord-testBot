c43cdc5f521e2229f4d23e50bbbe2b0f45b13822
Dominik Tröndle <domi.troendle@gmail.com>
2026-09-17 11:49:05 +0200

    feat: add commands to assign and remove roles

--- main.py
+++ main.py
@@ -27,6 +27,24 @@ async def greet(ctx):
 async def recent_message(ctx, arg):
     await ctx.send(f"Deine letzte Nachricht war: {arg}")
 
+@bot.command()
+async def assign(ctx):
+    role = discord.utils.get(ctx.guild.roles, name='Bot')
+    if not role:
+        await ctx.send("Die Rolle wurde nicht gefunden!")
+        return
+    await ctx.author.add_roles(role)
+    await ctx.send(f"{ctx.author.mention} wurde die Rolle '{role}' hinzugefügt!")
+
+@bot.command()
+async def remove(ctx):
+    role = discord.utils.get(ctx.guild.roles, name='Bot')
+    if not role:
+        await ctx.send("Die Rolle wurde nicht gefunden!")
+        return
+    await ctx.author.remove_roles(role)
+    await ctx.send(f"Die Rolle '{role}' wurde von {ctx.author.mention} entfernt!")
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
