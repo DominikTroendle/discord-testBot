dd98b633d8ef883f1a86e83d6c5f87d2b38bdeed
Dominik Tröndle <domi.troendle@gmail.com>
2026-09-17 08:51:00 +0200

    add discord.ext, basic bot commands and first event handler for on_ready event with bot.run

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import discord
 from dotenv import load_dotenv
 import os
-
+from discord.ext import commands
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready!")
+
+bot.run(token)
